02ab675b4483d1c47084a0e80f8769415838b5c3
Iury Silva de Almeida <iury.colombo@gmail.com>
2025-02-03 16:45:15 -0300

    Subindo montagem do projeto

---
+++ src/main/java/br/com/connectmotors/ConnectMotors/ConnectMotorsApplication.java
@@ -0,0 +1,13 @@
+package br.com.connectmotors.ConnectMotors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectMotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectMotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/connectmotors/ConnectMotors/ConnectMotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.connectmotors.ConnectMotors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectMotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
